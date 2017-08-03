e33f071522871390790d1e35235cba3b7fecf9ea
Yair Halberstadt <yairhalberstadt@gmail.com>
2017-08-03 20:12:39 +0100

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using IBApi;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EWrapperImpl
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            EWrapperImpl IBApi = new EWrapperImpl();
+            IBApi.ConnectToIB(7496, 1);
+            
+            IBApi.OpenOrder += OnOrder;
+            IBApi.ClientSocket.reqAllOpenOrders();
+            Console.Read();
+        }
+
+        static void OnOrder(object sender, OpenOrderArgs openOrderargs)
+        {
+            Console.WriteLine(openOrderargs.Order.OrderType);
+        }
+    }
+}
